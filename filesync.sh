a16bfd56c1ea32ba8d6d3754c9cd28f19ffe95f9
lindseypeng <lindsey.peng@mail.utoronto.ca>
2017-10-03 16:03:02 -0400

    Update filesync.sh

--- filesync.sh
+++ filesync.sh
@@ -7,15 +7,20 @@ echo "The script starts now"
 #this is a rsync script
 
 
-echo -e "Please enter output directory name:"
-read directory
-echo $directory
+#echo -e "Please enter input directory:"
+#read directory
 
 
-SOURCEDIR=yipgroup@192.168.181.171:/usr/local/image_store/Peng_Lindsey/
-DESTDIR=$directory
+#echo $directory
 
-rsync -avh $SOURCEDIR $DESTDIR
+
+SOURCEDIR=/home/alinsi/Desktop/profilesync
+DESTDIR=/home/alinsi/Desktop/profilesync2
+#DESTDIR=yipgroup@192.168.181.171:/usr/local/image_store/Peng_Lindsey/
+
+rsync -avhP --remove-source-files --log-file-format=%i --log-file=$DESTDIR/logfile.log $SOURCEDIR $DESTDIR
+
+#python {filename.py} argument1, argument 2
 
 
 
